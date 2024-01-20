1de5c79993127915f3dbe916c24ccc2472cf9b1c
donioscode <doniyornormuxammedov@gmail.com>
2024-01-20 22:25:01 +0500

    Destination file addeed

--- iTour/Destination.swift
+++ iTour/Destination.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+import SwiftData
+
+@Model
+class Destination {
+    var name: String
+    var details: String
+    let data: Data
+    var priority: Int
+    
+    init(name: String, details: String, data: Data, priority: Int) {
+        self.name = name
+        self.details = details
+        self.data = data
+        self.priority = priority
+    }
+}
--- iTour/iTourApp.swift
+++ iTour/iTourApp.swift
@@ -5,6 +5,7 @@
 //  Created by doniyor normuxammedov on 20/01/24.
 //
 
+import SwiftData
 import SwiftUI
 
 @main
@@ -13,5 +14,6 @@ struct iTourApp: App {
         WindowGroup {
             ContentView()
         }
+        .modelContainer(for: Destination.self)
     }
 }
