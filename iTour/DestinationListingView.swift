0eaf2f07a582b858befe801cf59769f94e9b1671
donioscode <doniyornormuxammedov@gmail.com>
2024-01-21 13:01:27 +0500

    DestinationLIstingView file added

---
+++ iTour/DestinationListingView.swift
@@ -0,0 +1,18 @@
+//
+//  DestinationListingView.swift
+//  iTour
+//
+//  Created by doniyor normuxammedov on 21/01/24.
+//
+
+import SwiftUI
+
+struct DestinationListingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DestinationListingView()
+}
